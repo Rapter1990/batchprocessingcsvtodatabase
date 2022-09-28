3ce8c17d42f3f6c7e2cbe0ba703a49e26a7e7dd7
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2022-09-28 17:03:01 +0300

    Task 8 : Define BatchController

---
+++ src/main/java/com/example/batchprocessingcsvtodatabase/controller/BatchController.java
@@ -0,0 +1,45 @@
+package com.example.batchprocessingcsvtodatabase.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/batch")// Root path
+@RequiredArgsConstructor
+@Slf4j
+public class BatchController {
+
+    private final JobLauncher jobLauncher;
+    private final Job job;
+
+    @PostMapping("/importuserjob")
+    public ResponseEntity<String> importCsvToDBJob() {
+
+        log.info("BatchController | importCsvToDBJob is called");
+
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addLong("startAt", System.currentTimeMillis()).toJobParameters();
+        try {
+            jobLauncher.run(job, jobParameters);
+        } catch (JobExecutionAlreadyRunningException | JobRestartException |
+                 JobInstanceAlreadyCompleteException | JobParametersInvalidException e) {
+            log.info("BatchController | importCsvToDBJob | error : " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        return new ResponseEntity<>("Batch Process started!!", HttpStatus.OK);
+    }
+}
